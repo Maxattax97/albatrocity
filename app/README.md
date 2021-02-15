3ba208a43b6e0f2f525a8f115afc7e8ae68428df
Max O'Cull <max.ocull@protonmail.com>
2021-02-14 23:04:35 -0500

    Start of Quasar app

---
+++ app/README.md
@@ -0,0 +1,26 @@
+# Albatrocity (albatrocity)
+
+Web utility to monitor your LAN and report status of nodes on the network 
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+npm run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ app/src/pages/Index.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="flex">
+      <div class="col">
+        <div class="flex-break q-py-xl"></div>
+        <div class="row justify-center">
+            <q-input clearable rounded outlined class="col-8"
+                placeholder="Search terms #tags">
+                <template v-slot:append>
+                    <q-avatar>
+                        <q-icon name="search" />
+                    </q-avatar>
+                </template>
+            </q-input>
+        </div>
+        <div class="flex-break q-py-md"></div>
+        <div class="row justify-center">
+            <q-list bordered separator>
+                <q-item clickable v-ripple :active="active">
+                    <q-item-section>
+                        <q-input value="TJ's Pi"/>
+                    </q-item-section>
+                    <q-item-section>192.168.25.3</q-item-section>
+                    <q-item-section side>Ubuntu</q-item-section>
+                    <q-item-section side>22, 23, 80, 443</q-item-section>
+                    <q-item-section avatar>
+                        <q-icon color="positive" name="mdi-lan" />
+                    </q-item-section>
+                </q-item>
+                <q-item clickable v-ripple :active="active">
+                    <q-item-section>10.2.26.211</q-item-section>
+                    <q-item-section side>Arch Linux</q-item-section>
+                    <q-item-section side>22, 443</q-item-section>
+                    <q-item-section avatar>
+                        <q-icon color="negative" name="mdi-lan" />
+                    </q-item-section>
+                </q-item>
+            </q-list>
+        </div>
+      </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+};
+</script>
